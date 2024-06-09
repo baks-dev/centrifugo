94b58da3fb8d0db3b97cec094f9a0b379b1c2388
kingstar <admin@baks.dev>
2024-06-10 00:10:38 +0300

    Baks Development

--- BaksDevCentrifugoBundle.php
+++ BaksDevCentrifugoBundle.php
@@ -22,4 +22,10 @@ class BaksDevCentrifugoBundle extends AbstractBundle
     public const NAMESPACE = __NAMESPACE__.'\\';
 
     public const PATH = __DIR__.DIRECTORY_SEPARATOR;
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $path = self::PATH.'Resources/packages/centrifugo/services.php';
+        $container->import($path);
+    }
 }
